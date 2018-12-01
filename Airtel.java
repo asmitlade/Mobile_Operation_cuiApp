f188d5006a139e66a54f87ff8da75457173db072
asmitlade <asmitlade.system@gmail.com>
2018-12-01 17:06:22 +0530

    craete Airtel.java aas subclass of Sim for implementing Sim methods

---
+++ Airtel.java
@@ -0,0 +1,8 @@
+class Airtel implements Sim {
+	public void makeCall(long mobNo){
+		System.out.println("Calling to ..."+ mobNo);
+	}
+	public void sendSms(String msg, long mobNo){
+		System.out.println("Send sms to..."+ mobNo);
+	}
+}
